d680121860eebfb89ae6e151c6ef34f2f9843296
bluesfools <bluesfools@users.noreply.github.com>
2026-08-03 12:59:46 +0200

    controller: implement login, root redirect, and user list

--- src/main/java/com/poseidoncapitalsolutions/poseidon/controller/LoginController.java
+++ src/main/java/com/poseidoncapitalsolutions/poseidon/controller/LoginController.java
@@ -1,4 +1,50 @@
 package com.poseidoncapitalsolutions.poseidon.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.poseidoncapitalsolutions.poseidon.repository.UserRepository;
+
+
+
+
+@Controller
+//@RequestMapping("app")
 public class LoginController {
+
+    private final UserRepository userRepository;
+
+    public LoginController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/")
+    public String redirectToLogin() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("login");
+        return mav;
+    }
+
+    @GetMapping("/secure/article-details")
+    public ModelAndView getAllUserArticles() {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("users", userRepository.findAll());
+        mav.setViewName("user/list");
+        return mav;
+    }
+
+    @GetMapping("/403")
+    public ModelAndView error() {
+        ModelAndView mav = new ModelAndView();
+        String errorMessage = "You are not authorized for the requested data.";
+        mav.addObject("errorMsg", errorMessage);
+        mav.setViewName("403");
+        return mav;
+    }
 }
