a92531464ce113a567cdfad6889e8c13408d8fd0
bluesfools <bluesfools@users.noreply.github.com>
2026-08-03 12:59:39 +0200

    controller: add /home and /admin/home endpoints

--- src/main/java/com/poseidoncapitalsolutions/poseidon/controller/HomeController.java
+++ src/main/java/com/poseidoncapitalsolutions/poseidon/controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.poseidoncapitalsolutions.poseidon.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller responsible for handling the home page request.
+ * Provides an endpoint for displaying the home page of the application.
+ */
 
 @Controller
 public class HomeController {
-    
-    @GetMapping("/")
+
+    @RequestMapping("/home")
     public String home() {
         return "home";
+    }
+
+    @RequestMapping("/admin/home")
+    public String adminHome() {
+        return "redirect:/bidList/list";
     }
 }
